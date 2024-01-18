b22e293f64092dcdd5d2fef77f5077a72299b017
Tan Yong Jian <64192594+Pakuluku@users.noreply.github.com>
2024-01-18 13:01:32 +0800

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -179,8 +179,5 @@ function copyText() {
   copyText.setSelectionRange(0, 99999); // For mobile devices
 
    // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
+  navigator.clipboard.writeText(copyText.value)
 }
